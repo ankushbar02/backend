e8ffff75fdad4628b47205e968b2154c10021e03
ankushbar02 <ankushbar02@gmail.com>
2023-10-23 23:40:25 +0530

    dfg

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -54,6 +54,7 @@ userRouter.post("/login", async (req, res) => {
             maxAge: maxAge * 1000,
             sameSite: 'None', // Allow cross-origin
             secure: true,     // Require HTTPS for cross-origin
+            domain:"https://stickyfrontend.onrender.com"
           })
           .status(201)
           .json({ userID: user._id });
@@ -81,7 +82,8 @@ userRouter.post("/signup", async (req, res) => {
       httpOnly: false,
       maxAge: maxAge * 1000,
       sameSite: 'None', // Allow cross-origin
-      secure: true,     // Require HTTPS for cross-origin
+      secure: true, 
+      domain:"https://stickyfrontend.onrender.com"
     });
     res.status(201).json({ userID: user._id });
   } catch (err) {
