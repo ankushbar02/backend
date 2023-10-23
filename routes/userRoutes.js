e8128e58dc13a10eb357e6bb2d64e7828589bba3
ankushbar02 <ankushbar02@gmail.com>
2023-10-24 03:10:42 +0530

    nm

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -50,9 +50,7 @@ userRouter.post("/login", async (req, res) => {
           .cookie("jwt", token, {
             httpOnly: false,
             maxAge: maxAge * 1000,
-            sameSite: "None", // Allow cross-origin
-            secure: true, // Require HTTPS for cross-origin
-           
+         
           })
           .status(201)
           .json({ userID: user._id });
@@ -78,9 +76,7 @@ userRouter.post("/signup", async (req, res) => {
     res.cookie("jwt", token, {
       httpOnly: false,
       maxAge: maxAge * 1000,
-      sameSite: "None", // Allow cross-origin
-      secure: true, // Require HTTPS for cross-origin
-     
+      
     });
     res.status(201).json({ userID: user._id });
   } catch (err) {
