60542e01cf2ce59252ad2dba29e9a872e03f0743
ankushbar02 <ankushbar02@gmail.com>
2023-10-24 20:51:29 +0530

    sd

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -45,8 +45,17 @@ userRouter.post("/login", async (req, res) => {
       const passwordMatch = await bcrypt.compare(password, user.password);
       if (passwordMatch) {
         const token = createToken(user._id);
-
-        res.status(201).json({ token });
+        
+        res
+          .cookie("jwt", token, {
+            httpOnly: false,
+            maxAge: maxAge * 1000,
+            sameSite: "None", 
+            secure: true, 
+           
+          })
+          .status(201)
+          .json({ token});
       } else {
         throw new Error("incorrect password");
       }
@@ -66,7 +75,13 @@ userRouter.post("/signup", async (req, res) => {
     const hashPassword = await bcrypt.hash(password, 10);
     const user = await User.create({ userName, password: hashPassword });
     const token = createToken(user._id);
-
+    res.cookie("jwt", token, {
+      httpOnly: false,
+      maxAge: maxAge * 1000,
+      sameSite: "None", 
+      secure: true,
+     
+    });
     res.status(201).json({ token });
   } catch (err) {
     const errors = handleErrors(err);
