b50b010e7137d2860cc271203f05362e50a1b5de
ankushbar02 <ankushbar02@gmail.com>
2023-10-24 18:21:47 +0530

    aef

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -52,7 +52,7 @@ userRouter.post("/login", async (req, res) => {
             maxAge: maxAge * 1000,
             sameSite: "None", // Allow cross-origin
             secure: true, // Require HTTPS for cross-origin
-            domain:"stickyfrontend.onrender.com"
+            domain:"frontend-seven-gold.vercel.app"
           })
           .status(201)
           .json({ userID: user._id });
@@ -80,7 +80,7 @@ userRouter.post("/signup", async (req, res) => {
       maxAge: maxAge * 1000,
       sameSite: "None", // Allow cross-origin
       secure: true, // Require HTTPS for cross-origin
-      domain:"stickyfrontend.onrender.com"
+      domain:"frontend-seven-gold.vercel.app"
     });
     res.status(201).json({ userID: user._id });
   } catch (err) {
