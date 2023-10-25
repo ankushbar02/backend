6d055a6fe001c6b3f37e184782ba0af249a7cc8c
ankushbar02 <ankushbar02@gmail.com>
2023-10-25 20:17:48 +0530

    fv

--- index.js
+++ index.js
@@ -1,53 +1,20 @@
-// import express from "express";
-// import cookieParser from "cookie-parser";
-// import mongoose from "mongoose";
-// import bodyParser from "body-parser";
-// import noteRouter from "./routes/NoteRoute.js";
-// import userRouter from "./routes/userRoutes.js";
-// import dotenv from "dotenv";
-// import cors from "cors";
-
-// dotenv.config();
-
-// const app = express();
-// mongoose.connect(process.env.DB || "mongodb://localhost:27017/NotesDB");
-
-// app.use(express.json());
-// app.use(cookieParser());
-
-// // Enable CORS for specific origin and allow credentials
-// app.use(
-//   cors({
-//     origin: process.env.CLIENT_WEB, // Replace with your client's actual domain
-//     credentials: true,
-//   })
-// );
-
-// // Define a route to handle preflight requests (OPTIONS requests)
-// app.options('*', cors());
-// app.use(userRouter);
-// app.use(noteRouter);
-// // Your route handlers and other middleware go here...
-
-// // Start the Express server
-// const port = process.env.PORT || 4000;
-// app.listen(port, () => {
-//   console.log(`Server is running on port ${port}`);
-// });
 import express from "express";
 import cookieParser from "cookie-parser";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import noteRouter from "./routes/NoteRoute.js";
 import userRouter from "./routes/userRoutes.js";
-import cors from "cors";
-import dotenv from "dotenv";
 
+import dotenv from "dotenv";
+const app = express();
 dotenv.config();
 
-const app = express();
 
 
+app.use(bodyParser.json());
+
+
+app.use(cookieParser());
 
 
 
@@ -87,22 +54,17 @@ app.options( `${process.env.CLIENT_WEB}/readnotes`, (req, res) => {
   res.status(204).send();
 });
 
-// Your other middleware and route handlers go here...
 
-// Connect to the MongoDB database
 mongoose.connect(process.env.DB || "mongodb://localhost:27017/NotesDB");
 
-// Use body-parser to parse incoming JSON requests
-app.use(bodyParser.json());
 
-// Use cookie-parser to parse incoming cookies
-app.use(cookieParser());
 
-// Use the noteRouter and userRouter to handle requests to /notes and /users respectively
+
+
 app.use( noteRouter);
 app.use( userRouter);
 
-// Start the Express server
+
 const port = process.env.PORT || 4000;
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
