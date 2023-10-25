a647f99d0c7c7e6694930dadf614f01577d6f6ec
ankushbar02 <ankushbar02@gmail.com>
2023-10-25 17:41:41 +0530

    k;l

--- index.js
+++ index.js
@@ -56,7 +56,14 @@ app.use(
 );
 
 // Define a route to handle preflight requests (OPTIONS requests)
-app.options("*", cors());
+app.options('*', (req, res) => {
+  res.header('Access-Control-Allow-Origin', process.env.CLIENT_WEB);
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.status(204).send();
+});
+
 
 // Your other middleware and route handlers go here...
 
