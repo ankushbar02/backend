218a52d7245d274da462e31593db7a382cd727d3
ankushbar02 <ankushbar02@gmail.com>
2023-10-26 18:12:05 +0530

    fn

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(cors(
   {
-      origin: ["https://deploy-mern-frontend.vercel.app"],
+      origin: ["https://stickynotes-mern.netlify.app/"],
       methods: ["POST", "GET","PATCH","UPDATE","DELETE"],
       credentials: true
   }
