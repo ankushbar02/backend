6aaa3513b59dcbce57147f9f5ea5fc65deecb6e3
ankushbar02 <ankushbar02@gmail.com>
2023-10-26 12:00:54 +0530

    afc

--- index.js
+++ index.js
@@ -12,24 +12,30 @@ import dotenv from "dotenv";
 const app = express();
 
 dotenv.config();
-
-
-app.use(cors({
-  origin:process.env.CLIENT_WEB,
-  credentials:true,
-    methods:["GET","POST","PATCH","DELETE","UPDATE"],
-    optionsSuccessStatus:200
-}))
-
-
-
-mongoose.connect(process.env.DB || "mongodb://localhost:27017/NotesDB");
-
 // Use body-parser to parse incoming JSON requests
 app.use(bodyParser.json());
 
 // Use cookie-parser to parse incoming cookies
 app.use(cookieParser());
+
+
+var whitelist = ['http:localhost:3000', process.env.CLIENT_WEB]
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  },
+  credentials:true,
+  allowedHeaders:["x-render-origin-server","Content-Type","x-powered-by","Server"]
+}
+ 
+app.use(cors(corsOptions))
+
+mongoose.connect(process.env.DB || "mongodb://localhost:27017/NotesDB");
+
 
 // Use the noteRouter and userRouter to handle requests to /notes and /users respectively
 app.use( noteRouter);
