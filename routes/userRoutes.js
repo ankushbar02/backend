afe686900b79a757d7dc8fbbf80baaef9eb1b4a8
ankushbar02 <ankushbar02@gmail.com>
2023-10-27 14:29:40 +0530

    vb

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -8,7 +8,7 @@ const userRouter = express.Router();
 userRouter.use(cookieParser());
 userRouter.use(express.urlencoded({ extended: true }));
 const maxAge = 3 * 24 * 60 * 60;
-dotenv.config();
+dotenv.config(); 
 const createToken = (id) => {
   return jwt.sign({ id }, process.env.SALT, { expiresIn: maxAge });
 };
